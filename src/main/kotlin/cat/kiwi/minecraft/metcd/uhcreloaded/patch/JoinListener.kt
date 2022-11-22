9ac0a2e60b44d065d00accf79ead291cfc9da901
moeKiwiSAMA <me@kiwi.cat>
2022-11-23 05:18:42 +0800

    player join event

--- src/main/kotlin/cat/kiwi/minecraft/metcd/uhcreloaded/patch/JoinListener.kt
+++ src/main/kotlin/cat/kiwi/minecraft/metcd/uhcreloaded/patch/JoinListener.kt
@@ -1,16 +1,20 @@
 package cat.kiwi.minecraft.metcd.uhcreloaded.patch
 
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerLoginEvent
 
 class JoinListener : Listener {
     @EventHandler
-    fun onPlayerLoginEvent(e: PlayerLoginEvent) {
+    fun onPlayerJoinEvent(e: PlayerJoinEvent) {
         if (e.player.hasPermission("uhcpatch.bypass")) return
-        val instance = UHCPatch
-        if (instance.uhcDM && instance.uhcPG) {
+        val instance = UHCPatch.instance
+        if (UHCPatch.uhcDM && UHCPatch.uhcPG) {
             e.player.kickPlayer(Config.notCompleteMsg)
+            instance.logger.info("kicked player ${e.player.name}")
         }
     }
 }
